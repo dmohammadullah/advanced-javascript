efeb75f36ab1fc9423a97812eb57bd5d66557700
Mohammad Ullah <mohammad.cse2290@gmail.com>
2021-08-11 04:37:58 +0600

    double vs triple

--- double-vs-triple-equal.js
+++ double-vs-triple-equal.js
@@ -1,5 +1,6 @@
 const first = 2;
 const second = "2";
+//if(first === second)  // check only value & type ta ke convert kore kasakasi kore dekhbe same naki
 if(first === second){    // check value and type
     console.log("Condition is true");
 }
