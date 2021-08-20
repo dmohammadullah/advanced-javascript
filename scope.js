3cbfe306740faba0671018a5bb4dce96ae9ddd1d
Mohammad Ullah <mohammad.cse2290@gmail.com>
2021-08-20 23:53:37 +0600

    ki moja intermediate js ses diya laisi hehehegit add .

---
+++ scope.js
@@ -0,0 +1,10 @@
+function sum (first, second){
+    let rusult = first + second;
+    if(result > 9){
+        var mood = "happy";
+        mood = "fishy";
+        mood = "funky";
+        mood = "cranky"
+        //console.log(mood);
+    }
+}
